6fdbc46b6c171e7d86b8b32049112d9238baf8c5
ikr0w <gkrow@protonmail.com>
2023-04-03 19:10:14 +0800

    refactor: export all named exports

--- src/utils/canvas/index.ts
+++ src/utils/canvas/index.ts
@@ -1,13 +1,9 @@
 import canvas from "canvas"
 
 canvas.registerFont("./assets/fonts/Roboto-Black.ttf", { family: "Roboto Bk" })
-canvas.registerFont("./assets/fonts/WorkSans-Regular.ttf", {
-	family: "Work Sans",
-})
+canvas.registerFont("./assets/fonts/WorkSans-Regular.ttf", { family: "Work Sans" })
 canvas.registerFont("./assets/fonts/Changa-One.ttf", { family: "ChangaOne" })
 
-import { createDetailedAxieCanvas } from "./createAxieCanvas"
-import { createAxieTeamCanvas, createBattleCanvas, createBattleStatsCanvas } from "./createBattleCanvas"
-import { createCardCanvas } from "./createCardCanvas"
-
-export { createCardCanvas, createBattleCanvas, createAxieTeamCanvas, createBattleStatsCanvas, createDetailedAxieCanvas }
+export * from "./createAxieCanvas"
+export * from "./createBattleCanvas"
+export * from "./createCardCanvas"
--- src/utils/parsers/index.ts
+++ src/utils/parsers/index.ts
@@ -1,19 +1,6 @@
-import { parseAddress, parseId } from "./parseAddress"
-import { parseAxieGenes } from "./parseAxieGene"
-import { parseBalance } from "./parseBalance"
-import { parseArenaBattle, parsePlayerBattles } from "./parseBattles"
-import { getRuneCharmsOverviewField, parseInventory } from "./parseInventory"
-import { parseCard, parseItem } from "./parseItem"
-
-export {
-	parseAddress,
-	parseId,
-	parseAxieGenes,
-	parseBalance,
-	parseArenaBattle,
-	parsePlayerBattles,
-	parseCard,
-	parseItem,
-	parseInventory,
-	getRuneCharmsOverviewField,
-}
+export * from "./parseAddress"
+export * from "./parseAxieGene"
+export * from "./parseBalance"
+export * from "./parseBattles"
+export * from "./parseInventory"
+export * from "./parseItem"
