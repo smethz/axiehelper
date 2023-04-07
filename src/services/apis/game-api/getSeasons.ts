567d8b8bb06caa29071b8421c5a4f60b6de501ba
ikr0w <gkrow@protonmail.com>
2023-04-07 17:34:36 +0800

    refactor(getSeasons): add force param & cache until season end

--- src/services/apis/game-api/getSeasons.ts
+++ src/services/apis/game-api/getSeasons.ts
@@ -27,28 +27,44 @@ export interface Season {
 interface APISeasonsParams {
 	limit?: number
 	offset?: number
+	force?: boolean // Whether to skip the cache check and request the API
 }
 
 export async function getSeasons(
-	{ limit, offset }: APISeasonsParams = { limit: 100, offset: 0 }
+	{ limit, offset, force }: APISeasonsParams = { limit: 100, offset: 0, force: false }
 ): Promise<Season[] | void> {
 	const cacheKey = `origin_seasons`
 
+	if (!force) {
+		const cachedEntry = await cache.get(cacheKey)
+
+		if (cachedEntry) return [...JSON.parse(cachedEntry)] as Season[]
+	}
+
 	const seasons = await GameAPI.get<APISeasonsResponse>(`/v2/seasons`, { params: { limit, offset } })
-		.then(async (response) => response.data._items)
+		.then((response) => response.data._items)
 		.catch(async (error: AxiosError) => {
 			logger.error(error, `GameAPI Error: ${error.response?.status} getSeasons`)
 
 			const cacheEntry = await cache.get(cacheKey)
 
-			if (cacheEntry) return [...JSON.parse(cacheEntry)]
+			if (cacheEntry) return [...JSON.parse(cacheEntry)] as Season[]
 
 			return
 		})
 
 	if (!seasons) return
 
-	await cache.set(cacheKey, JSON.stringify(seasons), "EX", DEFAULT_CACHE_EXPIRATION)
+	const seasonIds = seasons.map((season) => season.id ?? -Infinity)
+	const latestSeason = seasons.find((season) => season.id === Math.max(...seasonIds))
+
+	// Set the cache expiration till season ends
+	const nowInSeconds = Math.round(Date.now() / 1000)
+	const secondsTillSeasonEnd = latestSeason ? latestSeason.endedAt - nowInSeconds : DEFAULT_CACHE_EXPIRATION
+	// Duration is negative in off-season
+	const cacheExpiration = secondsTillSeasonEnd > 0 ? secondsTillSeasonEnd : DEFAULT_CACHE_EXPIRATION
+
+	await cache.set(cacheKey, JSON.stringify(seasons), "EX", cacheExpiration)
 
 	return seasons
 }
